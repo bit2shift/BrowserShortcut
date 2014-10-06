1d7ce5f5f0565b6335f705bd3455c68fc18abc03
bit2shift <bit2shift@gmail.com>
2014-10-06 17:55:45 +0100

    Now with a text box to type a URL

--- src/com/android/browser/BrowserActivity.java
+++ src/com/android/browser/BrowserActivity.java
@@ -1,27 +1,53 @@
 package com.android.browser;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class BrowserActivity extends Activity
 {
+	private static final String TITLE = "Browser Shortcut";
+	private static final Object TAG = new Object();
+
+	private class ClickListener implements DialogInterface.OnClickListener
+	{
+		public void onClick(DialogInterface dialog, int which)
+		{
+			//get textbox from dialog
+			EditText box = (EditText)((AlertDialog)dialog).getWindow().getDecorView().findViewWithTag(TAG);
+
+			//issue intent
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(box.getEditableText().toString()));
+			BrowserActivity.this.startActivity(Intent.createChooser(intent, TITLE));
+			BrowserActivity.this.finish();
+		}
+	}
+
+	private class CancelListener implements DialogInterface.OnCancelListener
+	{
+		public void onCancel(DialogInterface dialog)
+		{
+			BrowserActivity.this.finish();
+		}
+	}
+
 	@Override
 	public void onCreate(Bundle savedState)
 	{
 		super.onCreate(savedState);
-		
-		try
-		{
-			Intent intent = new Intent(Intent.ACTION_VIEW);
-			intent.setData(Uri.parse("http://www.google.com"));
-			this.startActivity(Intent.createChooser(intent, "Browser"));
-		}
-		catch(Exception e){}
-		finally
-		{
-			this.finish();
-		}
+		EditText textbox = new EditText(this);
+		textbox.setTag(TAG);
+
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		alert.setTitle(TITLE);
+		alert.setMessage("Enter URL");
+		alert.setView(textbox);
+		alert.setPositiveButton("Go", new ClickListener());
+		alert.setOnCancelListener(new CancelListener());
+		alert.show();
 	}
 }
